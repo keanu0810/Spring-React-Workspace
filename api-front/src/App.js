f326769db44fe08f0956b9fc33721c2d51ab49eb
keanu0810 <dsj1201@naver.com>
2024-08-16 18:22:15 +0900

    주소API

---
+++ api-front/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import AddressSearch from './AddressSeaerch';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>주소검색 api 사용하기</h1>
+      <AddressSearch/>
+    </div>
+  );
+}
+
+export default App;
--- spring-password/src/main/java/com/kh/mapper/Mapper.java
+++
@@ -1,8 +0,0 @@
-package com.kh.mapper;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public class Mapper {
-
-}
